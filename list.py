8f4904875e02fcaef98806cfc251de9eb4b99fe9
Antonin Kadrmas <antonin.kadrmas@epptec.eu>
2023-05-11 17:17:16 +0200

    acidentally add PV2020 again

--- list.py
+++ list.py
@@ -1,24 +1,22 @@
-
-import os
-import re
-import sys 
+import os, re, sys,pathlib
 import pyperclip
 
-dir_list = os.listdir("./")
-actual_file = sys.argv[0]
+if(sys.argv[1]==None): raise Exception("Path did not given")
+path = pathlib.Path(__file__).parent.resolve().joinpath(sys.argv[1])
+if(not os.path.exists(pathlib.Path(__file__).parent.resolve().joinpath(sys.argv[1]))): raise Exception("Entered folder does not exists")
+dir_list = os.listdir(path)
 dict = []
 new_index = 100
+string= ""
 for index, x in enumerate(dir_list):
     match = re.match(r'^([0-9]+)*(.+)$',x)
-    if x in actual_file:continue
+    if x in sys.argv[0]:continue
     if(match[1]==None):
         dict.append((new_index,x)) 
         new_index+=1
     else:
         dict.append((int(match[1]),x)) 
-
 dict.sort(key=lambda tup: tup[0]) 
-string= ""
 for index, x in enumerate(dict):
     string +=f"- [x] {x[1]}\n"
 pyperclip.copy(string)
